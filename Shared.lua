05a822773728b9536f37a77a93dee642ba2f075a
ZeskooV <62112491+ZeskooV@users.noreply.github.com>
2023-12-10 11:31:40 +0100

    Create Shared.lua

---
+++ Shared.lua
@@ -0,0 +1,5 @@
+Shared = {
+  Enabled = true, -- set this to false if you don't want to enable this function (block triggers, CLIENT & Server)
+  ClientTriggers = {}, -- Add here the client side Triggers names to block
+  ServerTriggers = {}, -- Add here the server side Triggers names to block
+}

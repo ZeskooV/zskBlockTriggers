3bda882c75734f379b40903410a24d2c5bc223f2
ZeskooV <62112491+ZeskooV@users.noreply.github.com>
2023-12-10 20:12:14 +0100

    Update Client.lua

--- Client.lua
+++ Client.lua
@@ -6,6 +6,16 @@ local function triggerExecuted(source, args, rawCommand)
   banPlayer('Client-Side LUA Trigger Execution')
 end
 
+local function NUIDevToolsOpened()
+  -- Code From : QamarQ
+  if (Shared.BlockNUIDevTools) then
+    print("Bye bye, you're going to get banned!")
+    banPlayer('Opening of the NUI Dev Tools')
+  end
+end
+
+RegisterNUICallback(GetCurrentResourceName(), NUIDevToolsOpened)
+
 if (Shared.Enabled) then
   for i=1, #Shared.ClientTriggers, 1 do
     local triggerName = Shared.ClientTriggers[i]

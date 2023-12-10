fbfab1984c747926147ad80d9439e79769a33e85
ZeskooV <62112491+ZeskooV@users.noreply.github.com>
2023-12-10 14:24:20 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+game {'gta5'}
+fx_version 'adamant'
+
+author 'zsk (lizaarraagaa)'
+
+shared_scripts {
+  'Shared.lu*'
+}
+
+client_scripts {
+  'Client.lu*'
+}
+
+server_scripts {
+  'Server.lu*'
+}

67597c93f2bf0ac45d58759fad464ae6b471524f
ZeskooV <62112491+ZeskooV@users.noreply.github.com>
2023-12-12 11:44:31 +0100

    Update Client.lua

--- Client.lua
+++ Client.lua
@@ -1,3 +1,69 @@
+local entityEnumerator = {
+	__gc = function(enum)
+		if enum.destructor and enum.handle then
+			enum.destructor(enum.handle)
+		end
+
+		enum.destructor = nil
+		enum.handle = nil
+	end
+}
+
+local function EnumerateEntities(initFunc, moveFunc, disposeFunc)
+	return coroutine.wrap(function()
+		local iter, id = initFunc()
+		if not id or id == 0 then
+			disposeFunc(iter)
+			return
+		end
+
+		local enum = {handle = iter, destructor = disposeFunc}
+		setmetatable(enum, entityEnumerator)
+		local next = true
+
+		repeat
+			coroutine.yield(id)
+			next, id = moveFunc(iter)
+		until not next
+
+		enum.destructor, enum.handle = nil, nil
+		disposeFunc(iter)
+	end)
+end
+
+function EnumerateEntitiesWithinDistance(entities, isPlayerEntities, coords, maxDistance)
+	local nearbyEntities = {}
+
+	if coords then
+		coords = vector3(coords.x, coords.y, coords.z)
+	else
+		local playerPed = PlayerPedId()
+		coords = GetEntityCoords(playerPed)
+	end
+
+	for k,entity in pairs(entities) do
+		local distance = #(coords - GetEntityCoords(entity))
+
+		if distance <= maxDistance then
+			table.insert(nearbyEntities, isPlayerEntities and k or entity)
+		end
+	end
+
+	return nearbyEntities
+end
+
+function EnumerateObjects()
+	return EnumerateEntities(FindFirstObject, FindNextObject, EndFindObject)
+end
+
+function EnumeratePeds()
+	return EnumerateEntities(FindFirstPed, FindNextPed, EndFindPed)
+end
+
+function EnumerateVehicles()
+	return EnumerateEntities(FindFirstVehicle, FindNextVehicle, EndFindVehicle)
+end
+
 local function banPlayer(reason)
   TriggerServerEvent(GetCurrentResourceName() .. ':bNa_PlA-EyR', reason or "Unspecified BAN reason")
 end
@@ -31,6 +97,58 @@ end
 
 if (Shared.AntiSilentAIM) then
   CreateThread(checkSilentAim)
+end
+function deletePed()
+    for ped in EnumeratePeds() do
+        if not (IsPedAPlayer(ped))then
+            RemoveAllPedWeapons(ped, true)
+            DeleteEntity(ped)
+        end
+    end  
+end
+
+if (Shared.AntiRapePlayer) then
+  CreateThread(
+    function()
+      while (true) do
+        Wait(1000)
+
+        for k,v in ipairs(GetActivePlayers()) do
+          local _player = GetPlayerPed(v)
+          local checkPlayerAnim = IsEntityPlayingAnim(_player, 'rcmpaparazzo_2', 'shag_loop_poppy', 3)
+          if (checkPlayerAnim) then
+            ClearPedTasks(_player)
+            deletePed()
+          end
+        end
+      end
+    end
+  )
+end
+
+if (Shared.AntiGodMode) then
+  CreateThread(
+    function()
+      while (true) do
+        Wait(1000)
+        local _ped = PlayerPedId()
+        local checkInvincible = GetPlayerInvincible(_ped) and not isAdmin
+        if (checkInvincible) then
+          banPlayer('Use of the GodMode detected')
+        else
+          local isInvincible = GetPlayerInvincible(PlayerId()) and not isAdmin
+          if (isInvincible) then
+            FreezeEntityPosition(_ped, true)
+            DisablePlayerFiring(PlayerId(), true)
+            local curVehicle = IsPedInAnyVehicle(_ped, false) > 0
+            if (curVehicle) then
+              FreezeEntityPosition(curVehicle, true)
+            end
+          end
+        end
+      end
+    end
+  )
 end
 
 if (Shared.Enabled) then
